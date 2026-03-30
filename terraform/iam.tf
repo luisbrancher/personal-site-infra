9f2767f420d9d947102f835ebe3a04d15fa61368
Luis Brancher <luisbrancher@proton.me>
2026-03-30 22:39:06 +0300

    fix(iam): add missing permissions for apply and destroy operations

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,28 +3,46 @@ resource "aws_iam_group" "terraform_group" {
   name = "terraform-provisioners-group"
 }
 
-# define as politicas minimas de grupo para EC2 + SG
+# define as politicas minimas de grupo para EC2 + SG + IAM
 resource "aws_iam_policy" "ec2_minimal" {
   name        = "terraform-ec2-minimal"
-  description = "Permissões mínimas para EC2 + SG"
+  description = "Permissões mínimas para EC2 + SG + IAM (apply e destroy)"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "EC2BasicManagement"
+        Sid    = "EC2Management"
         Effect = "Allow"
         Action = [
+          # instancia
           "ec2:RunInstances",
           "ec2:TerminateInstances",
           "ec2:StartInstances",
           "ec2:StopInstances",
+          # vpc
           "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:ModifyVpcAttribute",
+          # subnet
           "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:ModifySubnetAttribute",
+          # routing
           "ec2:CreateRouteTable",
+          "ec2:DeleteRouteTable",
+          "ec2:CreateRoute",
+          "ec2:DeleteRoute",
+          "ec2:AssociateRouteTable",
+          "ec2:DisassociateRouteTable",
+          # gateway
           "ec2:CreateInternetGateway",
+          "ec2:DeleteInternetGateway",
           "ec2:AttachInternetGateway",
-          "ec2:AssociateRouteTable"
+          "ec2:DetachInternetGateway",
+          # tags
+          "ec2:CreateTags",
+          "ec2:DeleteTags"
         ]
         Resource = "*"
       },
@@ -60,13 +78,46 @@ resource "aws_iam_policy" "ec2_minimal" {
         Resource = "*"
       },
       {
+        Sid    = "IAMManagement"
+        Effect = "Allow"
+        Action = [
+          # grupo
+          "iam:CreateGroup",
+          "iam:DeleteGroup",
+          "iam:GetGroup",
+          # policy
+          "iam:CreatePolicy",
+          "iam:DeletePolicy",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:ListPolicyVersions",
+          "iam:TagPolicy",
+          "iam:UntagPolicy",
+          # usuario
+          "iam:CreateUser",
+          "iam:DeleteUser",
+          "iam:GetUser",
+          "iam:TagUser",
+          "iam:UntagUser",
+          # membership
+          "iam:AddUserToGroup",
+          "iam:RemoveUserFromGroup",
+          "iam:ListGroupsForUser",
+          # attachment
+          "iam:AttachGroupPolicy",
+          "iam:DetachGroupPolicy",
+          "iam:ListAttachedGroupPolicies"
+        ]
+        Resource = "*"
+      },
+      {
         Sid      = "DenyNonFreeTierInstances"
         Effect   = "Deny"
         Action   = "ec2:RunInstances"
         Resource = "*"
-        "Condition" : {
-          StringNotEquals : {
-            "ec2:InstanceType" : "t4g.micro"
+        Condition = {
+          StringNotEquals = {
+            "ec2:InstanceType" = "t4g.micro"
           }
         }
       }
@@ -74,7 +125,7 @@ resource "aws_iam_policy" "ec2_minimal" {
   })
 }
 
-# atribui as politicas de gruop (ec2 + sg)
+# atribui as politicas de gruop
 resource "aws_iam_group_policy_attachment" "ec2_minimal_attach" {
   group      = aws_iam_group.terraform_group.name
   policy_arn = aws_iam_policy.ec2_minimal.arn
